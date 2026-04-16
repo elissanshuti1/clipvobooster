de7c52e805732f024de90f8f0aa74e304830d3d4
elissanshuti1 <elissanshuti1@gmail.com>
2026-04-16 13:05:58 +0200

    fixed eslint errors

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const url = req.nextUrl.clone();
   const token = req.cookies.get("token");
   const adminToken = req.cookies.get("admin_token");
